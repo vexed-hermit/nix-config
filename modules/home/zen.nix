30bee43029e2f181c76e931bd81e5f1d060056d2
kingro27 <kumarvasu2006@gmail.com>
2026-08-09 20:58:33 +0545

    added pinned tabs declaratively to zen.nix

--- modules/home/zen.nix
+++ modules/home/zen.nix
@@ -22,5 +22,33 @@
         "sponsorBlocker@ajay.app" = "sponsorblock";
       };
     };
+
+    profiles.default = {
+      pinsForce = true;
+      pinsForceAction = "remove";
+
+      pins = {
+        "YouTube" = {
+          id = "a77145fa-8d77-40ed-b94b-63b3a2ada907";
+          url = "https://youtube.com";
+          position = 100;
+        };
+        "Gemini" = {
+          id = "85f54438-58c2-4524-9adc-4c02b6fd57bd";
+          url = "https://gemini.google.com";
+          position = 200;
+        };
+        "Claude" = {
+          id = "ebf62078-852d-481f-8fa3-7eb21f82a351";
+          url = "https://claude.ai";
+          position = 300;
+        };
+        "Reddit" = {
+          id = "e49da172-b97c-4f7c-a35c-8df4fa155d1d";
+          url = "https://reddit.com";
+          position = 400;
+        };
+      };
+    };
   };
 }
