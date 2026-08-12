463362c28fba4c54a9983a9a234c68959ee1d481
kingro27 <kumarvasu2006@gmail.com>
2026-08-12 07:06:02 +0545

    undid a change in kde-plasma.nix

--- hosts/tardis/home-tweaks.nix
+++
@@ -1,8 +0,0 @@
-{ ... }:
-
-{
-  programs.plasma.configFile.kcminputrc."Libinput/1739/52967/SYNA32EB:00 06CB:CEE7 Touchpad" = {
-    ClickMethod = 2;
-    NaturalScroll = true;
-  };
-}
--- modules/home/kde-plasma.nix
+++ modules/home/kde-plasma.nix
@@ -140,7 +140,14 @@ in
       };
 
       configFile = {
-        kcminputrc.Mouse.X11LibInputXAccelProfileFlat = true;
+        # Input Configuration
+        kcminputrc = {
+          "Libinput/1739/52967/SYNA32EB:00 06CB:CEE7 Touchpad" = {
+            ClickMethod = 2;
+            NaturalScroll = true;
+          };
+          Mouse.X11LibInputXAccelProfileFlat = true;
+        };
 
         # Display & Window Manager
         kwinrc = {
