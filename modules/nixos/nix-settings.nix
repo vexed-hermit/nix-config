55159e3578cd296dc7f5f7e3c77b8ca35a93821b
vexed-hermit <kumarvasu2006@gmail.com>
2026-09-03 14:00:33 +0545

    setting up nix-ld

--- modules/nixos/nix-settings.nix
+++ modules/nixos/nix-settings.nix
@@ -1,9 +1,19 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
+  ];
+
+  programs.nix-ld.enable = true;
+
+  programs.nix-ld.libraries = with pkgs; [
+    stdenv.cc.cc
+    zlib
+    openssl
+    glib
+    # add more as you hit "missing library" errors
   ];
 
   nixpkgs.config.allowUnfree = true;
