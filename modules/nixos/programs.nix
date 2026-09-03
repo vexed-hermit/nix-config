83d396350eb7573f5ba198cfa841e22cd7ab3c3c
vexed-hermit <kumarvasu2006@gmail.com>
2026-09-03 22:57:26 +0545

    install Steam

--- modules/nixos/programs.nix
+++ modules/nixos/programs.nix
@@ -8,6 +8,23 @@
     flake = "/home/${config.hostSettings.primaryUser}/nix-config";
   };
 
+  programs.steam = {
+    enable = true;
+
+    # Integrates Protontricks directly with Steam's environment
+    protontricks.enable = true;
+
+    # Declaratively installs Proton-GE and exposes it to Steam
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
+
+    # Optional: Open firewall ports for Steam features
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+  };
+
   programs.neovim.enable = true;
   programs.kdeconnect.enable = true;
 
