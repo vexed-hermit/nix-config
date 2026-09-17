d24610c8b328df47537492e37ee93231db00705e
vexed-hermit <kumarvasu2006@gmail.com>
2026-09-17 22:36:01 +0545

    nix override

--- modules/nixos/sops-nix.nix
+++ modules/nixos/sops-nix.nix
@@ -2,6 +2,15 @@
 {
   imports = [ inputs.sops-nix.nixosModules.sops ];
 
+  # TEMP: sops-nix's pkgs/sops-install-secrets/default.nix still requests
+  # buildGo125Module, removed from nixpkgs 2026-09-15 (Go 1.25 EOL).
+  # Drop once https://github.com/Mic92/sops-nix/pull/984 merges.
+  nixpkgs.overlays = [
+    (_: prev: {
+      buildGo125Module = prev.buildGoModule;
+    })
+  ];
+
   sops.defaultSopsFile = ../../secrets/${hostname}.yaml;
 
   sops.age.sshKeyPaths = [ "/persist/etc/ssh/ssh_host_ed25519_key" ];
