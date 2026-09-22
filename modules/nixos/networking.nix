c006b221467ecdc409015ac51f062eba866ec539
vexed-hermit <kumarvasu2006@gmail.com>
2026-09-22 22:25:56 +0545

    trying something out

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -1,6 +1,6 @@
 { config, ... }:
 {
-  networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
+  ## networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
 
   networking.networkmanager = {
     enable = true;
